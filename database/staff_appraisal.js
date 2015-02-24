fc53a24a0a86157a44d6e3a47de052a87c630b76
shakdwipeea <ashakdwipeea@gmail.com>
2015-02-23 21:34:05 -0800

    Staff login done. Now merge the new ui and set the data accordingly.

--- database/staff_appraisal.js
+++ database/staff_appraisal.js
@@ -3,7 +3,37 @@
  */
 module.exports = function (id , password, callback) {
     //todo check the id & password and pass the relevant data
-    callback({
-        error: false
+
+    var mysql = require('mysql');
+    var _ = require('underscore');
+
+    var params = require('./conn');
+
+    var connection = mysql.createConnection(params);
+
+    var query = "SELECT * FROM teacher_details WHERE teacher_id = ? AND password = ?";
+
+    var query = mysql.format(query, [id, password]);
+
+    var data = {
+        error: true
+    }
+
+    connection.query(query, function (err, rows) {
+
+        if (err) {
+            data.error = err;
+        }
+
+        if (rows[0]) {
+            data.error = false;
+            _.extend(data, rows);
+        }
+        else {
+            data.error = 'No column found'
+        }
+
+        callback(data);
     });
 }
+
