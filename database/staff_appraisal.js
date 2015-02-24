96d87a9646f8c6537e17088592391b7898e97ee7
shakdwipeea <ashakdwipeea@gmail.com>
2015-02-24 13:02:09 +0530

    Server for staff_login done . Receiving all the data .

--- database/staff_appraisal.js
+++ database/staff_appraisal.js
@@ -6,6 +6,9 @@ module.exports = function (id , password, callback) {
 
     var mysql = require('mysql');
     var _ = require('underscore');
+    var events = require('events');
+
+    var eventEmitter = new events.EventEmitter();
 
     var params = require('./conn');
 
@@ -16,8 +19,14 @@ module.exports = function (id , password, callback) {
     var query = mysql.format(query, [id, password]);
 
     var data = {
-        error: true
+        error: true,
+        result: []
     }
+
+    var count = 0,
+        countDone = 0;
+
+    var classes = [];
 
     connection.query(query, function (err, rows) {
 
@@ -28,12 +37,75 @@ module.exports = function (id , password, callback) {
         if (rows[0]) {
             data.error = false;
             _.extend(data, rows);
+
+
+            for(var i = 1; i < 12; i++)
+            {
+                if(rows[0]['class_' + i])
+                {
+                    classes.push({
+                        clas: 'class_' + i,
+                        table: rows[0]['class_' + i]
+                    });
+                    count++;
+                }
+            }
+
+            _.map(classes, function (clas) {
+                getSubjectDetails(clas);
+            });
+
         }
         else {
             data.error = 'No column found'
         }
 
+    });
+
+    eventEmitter.on('done', function () {
         callback(data);
     });
+
+     function getSubjectDetails(clas)
+     {
+         var subCode = clas.table;
+
+         if(subCode.substr(-1,1) === 'l')
+         {
+             //Lab Class
+             subCode = subCode.slice(10,-1);
+         }
+         else
+         {
+             //Theory Class
+             subCode = subCode.slice(8);
+         }
+
+         var query = 'SELECT * FROM subject_name WHERE subject_code = ?';
+         query = mysql.format(query, [subCode]);
+
+         connection.query(query, function (err, rows) {
+             if(err)
+             {
+                 data.error = err;
+                 eventEmitter.emit('done');
+             }
+             else
+             {
+                 _.extend(clas, rows[0]);
+                 data.result = classes;
+             }
+
+             countDone++;
+
+             if(countDone === count)
+             {
+                 eventEmitter.emit('done');
+             }
+         });
+
+
+     }
+
 }
 
